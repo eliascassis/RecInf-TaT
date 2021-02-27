be66bd9483854ba16998d0746d00b1089a9e9a3d
Elias Cyrino <eliascyrino@gmail.com>
2021-02-27 15:05:08 -0300

    in file changed to predicted_news_data.csv

--- scripts/auxiliar_scripts/prepare_database_data.py
+++ scripts/auxiliar_scripts/prepare_database_data.py
@@ -1,14 +1,14 @@
 ### Imports
 from pandas import read_csv,read_json
 
-# Reading csv file ../../data/processed_meta_information.csv
-meta_file = read_csv('../../data/processed_meta_information.csv')
+# Reading csv file ../../data/predicted_news_data.csv
+meta_file = read_csv('../../data/predicted_news_data.csv')
 
 # Selecting the required data
 news_text_list = []
 for index, row in meta_file.iterrows():
     index_at_directory = row['index at directory'].split('-')
-    label = row['label']
+    label = 'true' if row['label'] else 'fake'
     directory_path = f"../../data/fake_br/full_texts/{label}/{index_at_directory[0]}.txt"
     file = open(directory_path,'r')
     meta_file.loc[meta_file.index[row['index']], 'full text'] = file.read()
